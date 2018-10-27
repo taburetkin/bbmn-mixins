644cd34c377b1057e06631253815e402714e3487
dimaver <dima.taburetkin@gmail.com>
2018-10-27 23:04:37 +0300

    saveAsPromise - added patchInsteadSave option

--- src/model/save-as-promise/index.js
+++ src/model/save-as-promise/index.js
@@ -2,6 +2,7 @@ import _ from 'underscore';
 export default Base => Base.extend({
 	defaultWait: false,
 	saveReturnPromise: false,
+	patchInsteadSave: false,
 	save(key, val, options){
 		let attrs;
 		if (key == null || typeof key === 'object') {
@@ -14,7 +15,9 @@ export default Base => Base.extend({
 		if(!_.has(options, 'wait')){
 			options.wait = this.defaultWait;
 		}
-
+		if(!_.has(options, 'patch')) {
+			options.patch = this.patchInsteadSave;
+		}
 		const save = Base.prototype.save.call(this, attrs, options);
 		if (!this.saveReturnPromise) {
 			return save;
