e41d4b3513baa394f350612a330160fb7916be68
dimaver <dima.taburetkin@gmail.com>
2018-11-12 10:19:44 +0300

    added scrollHandlerMixin

--- src/view/index.js
+++ src/view/index.js
@@ -2,7 +2,7 @@ import buildViewByKeyMixin from './build-view-by-key/index.js';
 import cssClassModifiersMixin from './css-class-modifiers/index.js';
 import destroyViewMixin from './destroy-view/index.js';
 import nestedViewsMixin from './nested-views/index.js';
-
+import scrollHandlerMixin from './scroll-handler';
 export {
-	cssClassModifiersMixin, nestedViewsMixin, destroyViewMixin , buildViewByKeyMixin
+	cssClassModifiersMixin, nestedViewsMixin, destroyViewMixin , buildViewByKeyMixin, scrollHandlerMixin
 };
