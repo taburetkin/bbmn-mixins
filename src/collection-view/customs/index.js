d64e5238b5cd05ae7a04d33abd08ac61e8cf7284
dimaver <dima.taburetkin@gmail.com>
2018-12-10 21:56:37 +0300

    customsMixin buildView now can accept options as a function

--- src/collection-view/customs/index.js
+++ src/collection-view/customs/index.js
@@ -114,7 +114,8 @@ export default Base => Base.extend({
 					options = view.options;
 				view = view.view;
 			} else if(isViewClass(view.view)) {
-				let viewOptions = _.extend({}, childOptions, view.options);
+				let options = betterResult(view, 'options', { context: this, args: [this, childOptions]})
+				let viewOptions = _.extend({}, childOptions, options);
 				view = new view.view(viewOptions);
 			}
 		}
